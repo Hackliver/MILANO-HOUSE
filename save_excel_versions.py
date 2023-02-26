bcb6fa4a8e10ae7849d0a07029ae667e950b8eda
tommaso.ramella <tommaso.ramella90@gmail.com>
2023-02-27 00:18:23 +0100

    fixes

--- save_excel_versions.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import os
-
-version = len(os.listdir("file_version_control"))
-df = pd.read_parquet('milano_housing_price_clean.parquet.gzip', engine='fastparquet')
-df.to_excel(f'file_version_control/milano_housing_{version+1}.xlsx', engine='xlsxwriter')
-
-
-#%%
